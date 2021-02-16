8c7bd9cd0fe746d640361d4c6e1cdd14834044c2
aleksandrshliakhovoi <aleksandrshliakhovoi@gmail.com>
2021-02-16 12:23:36 +0200

    add api response

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,5 +7,28 @@ class HomeController < ApplicationController
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @output = JSON.parse(@response)
+    #@final_output = @output[0]['AQI']
+    #Check for empty results
+    # if @output.empty?
+    #   @final_output = "Error"
+    # elsif !@output
+    #   @final_output = "Error"
+    # else
+    #   @final_output = @output[0]['AQI']
+    # end
+    
+    @final_output = @output[0]['AQI']
+
+    if @final_output <= 50 
+      @api_color = 'green'
+    elsif @final_output >= 51 && @final_output <= 100
+      @api_color = 'yellow'
+    elsif @final_output >= 101 && @final_output <= 150
+      @api_color = 'orange'
+    elsif @final_output >= 151 && @final_output <= 200
+      @api_color = 'red'
+    elsif @final_output >= 201
+      @api_color = 'purple'
+    end 
   end
 end
