ff868087b4c2fe929b762b6b7ab7a9b04b3472b5
aleksandrshliakhovoi <aleksandrshliakhovoi@gmail.com>
2021-02-16 03:27:47 +0200

    add api logic

--- Gemfile
+++ Gemfile
@@ -10,6 +10,10 @@ gem 'sass-rails', '>= 6'
 gem 'webpacker', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
+gem 'bootstrap', '~> 4.6'
+gem 'jquery-rails'
+gem 'json', '~> 2.5', '>= 2.5.1'
+
 
 
 gem 'bootsnap', '>= 1.4.4', require: false
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=80014&distance=0&API_KEY=BAC763AF-F21B-40D8-AA7B-3CB2EAED5765'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
   end
 end
